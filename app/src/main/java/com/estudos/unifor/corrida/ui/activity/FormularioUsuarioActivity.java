1dfdb9529b22cec6d308d5c4dff04823141c398b
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2019-09-19 23:58:57 -0300

    utilizando o banco para salvar usuario

--- app/src/main/java/com/estudos/unifor/corrida/ui/activity/FormularioUsuarioActivity.java
+++ app/src/main/java/com/estudos/unifor/corrida/ui/activity/FormularioUsuarioActivity.java
@@ -1,6 +1,7 @@
 package com.estudos.unifor.corrida.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,14 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.estudos.unifor.corrida.R;
+import com.estudos.unifor.corrida.dao.RoomUsuarioDAO;
 import com.estudos.unifor.corrida.dao.UsuarioDAO;
+import com.estudos.unifor.corrida.database.CorridaDatabase;
 import com.estudos.unifor.corrida.model.Usuario;
 
 public class FormularioUsuarioActivity extends AppCompatActivity {
+
+    private RoomUsuarioDAO dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,35 +26,30 @@ public class FormularioUsuarioActivity extends AppCompatActivity {
 
         setTitle("Inscrever-se");
 
-        final UsuarioDAO dao = new UsuarioDAO();
+        CorridaDatabase database = Room
+                .databaseBuilder(this, CorridaDatabase.class, "corrida.db")
+                .allowMainThreadQueries()
+                .build();
+
+        dao = database.getRoomUsuarioDAO();
 
         final TextView campoNome = findViewById(R.id.activity_formulario_usuario_nome);
-        final View campoFeminino = findViewById(R.id.activity_formulario_usuario_genero_feminino);
-        final View campoMasculino = findViewById(R.id.activity_formulario_usuario_genero_masculino);
         final TextView campoEmail = findViewById(R.id.activity_formulario_usuario_email);
         final TextView campoSenha = findViewById(R.id.activity_formulario_usuario_senha);
-        final TextView campoTelefone = findViewById(R.id.activity_formulario_usuario_telefone);
-        final TextView campoDisciplina = findViewById(R.id.activity_formulario_usuario_disciplina);
-        final TextView campoTurma = findViewById(R.id.activity_formulario_usuario_turma);
-
 
         Button botaoSalvar = findViewById(R.id.activity_formulario_usuario_botao_registrar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               String nome = campoNome.getText().toString();
 
+               String nome = campoNome.getText().toString();
                String email = campoEmail.getText().toString();
                String senha = campoSenha.getText().toString();
-               String telefone = campoTelefone.getText().toString();
-               String disciplina = campoDisciplina.getText().toString();
-               String turma = campoTurma.getText().toString();
 
                Usuario usuarioCriado = new Usuario(nome, email, senha);
                dao.salva(usuarioCriado);
 
                finish();
-
             }
         });
     }
--- app/src/main/java/com/estudos/unifor/corrida/ui/activity/LoginActivity.java
+++ app/src/main/java/com/estudos/unifor/corrida/ui/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.estudos.unifor.corrida.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.estudos.unifor.corrida.R;
 
@@ -14,6 +16,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         setTitle("Log in");
+    }
 
+    public void registrar(View view){
+        Intent intent = new Intent(this, FormularioUsuarioActivity.class);
+        startActivity(intent);
     }
 }
